957f6aa07dc3ef3131a582630a82d55cf2e84755
Sachinyaduvanshi7 <sachinyrao7@gmail.com>
2024-02-20 15:47:27 +0530

    1

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This is my first react app.
+          This is my first react app by using codepipeline.
         </p>
         <a
           className="App-link"
