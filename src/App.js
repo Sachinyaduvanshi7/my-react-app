77bbdca17857fb10583382162a6ce3e27284b437
SACHIN YADAV <132833023+Sachinyaduvanshi7@users.noreply.github.com>
2024-12-30 17:35:09 +0530

    Update App.js-------1

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This is my first react app using codepipeline test-1.
+          This is my first react app using codepipeline test-2.
         </p>
         <a
           className="App-link"
