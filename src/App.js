fd7e6ad3b8de33ea3c551b91c10ebd17ec9e9ae3
SACHIN YADAV <132833023+Sachinyaduvanshi7@users.noreply.github.com>
2025-08-08 11:25:50 +0530

    Update App.js in jen

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This is my first react app using codepipeline test-3 final-5.
+          This is my first react app using codepipeline using jenkins.
         </p>
         <a
           className="App-link"
